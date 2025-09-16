97782bb87e26e00e0464133f33c28ce1a54af04a
ilaypaz <pazlilay@gmail.com>
2025-09-16 14:19:03 -0700

    Update Hotel.java

--- Hotel Project/Hotel.java
+++ Hotel Project/Hotel.java
@@ -1,28 +1,6 @@
  package com.example.demo1;
 import java.util.ArrayList;
-
-/**
- * A class that represents a one room hotel and allows reservations to be made
- * and cancelled for the month of December.
- * 
- *
- */
-
-
-//888888ba                                  dP                     dP oo   dP   
-//88    `8b                                 88                     88      88   
-//88     88 .d8888b.    88d888b. .d8888b. d8888P    .d8888b. .d888b88 dP d8888P 
-//88     88 88'  `88    88'  `88 88'  `88   88      88ooood8 88'  `88 88   88   
-//88    .8P 88.  .88    88    88 88.  .88   88      88.  ... 88.  .88 88   88   
-//8888888P  `88888P'    dP    dP `88888P'   dP      `88888P' `88888P8 dP   dP   
-//                                                                              
-//                                                                              
-//  dP   dP       oo             .8888b oo dP                                   
-//  88   88                      88   "    88                                   
-//d8888P 88d888b. dP .d8888b.    88aaa  dP 88 .d8888b.                          
-//  88   88'  `88 88 Y8ooooo.    88     88 88 88ooood8                          
-//  88   88    88 88       88    88     88 88 88.  ...                          
-//  dP   dP    dP dP `88888P'    dP     dP dP `88888P'                          
+             
                                                                               
                                                                               
 
@@ -125,5 +103,6 @@ public class Hotel {
 	}
 
 }
+
 
 
